bc5611e0f4a9a6cceecb44a7ffe2f5033aca255c
Norbert2711 <norbertu2711@gmail.com>
2021-01-31 17:36:13 +0100

    heroku

--- src/main/java/com/crud2/domain/Task.java
+++ src/main/java/com/crud2/domain/Task.java
@@ -14,7 +14,8 @@ import javax.persistence.*;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
